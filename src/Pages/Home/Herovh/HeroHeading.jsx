f32a9265eb1fb227318f55ce808431926da76c07
Ayush6287 <152904397+Ayush6287@users.noreply.github.com>
2026-02-13 19:30:53 +0530

    Updated hero section with GSAP animation and layout fixes

--- src/Pages/Home/Herovh/HeroHeading.jsx
+++ src/Pages/Home/Herovh/HeroHeading.jsx
@@ -38,12 +38,15 @@ const HeroHeading = () => {
                        text-3xl sm:text-4xl md:text-4xl lg:text-6xl">
 
           <span className="hero-line block">
-            From Concept to Production  — Rapidly.    We Build   Modern Apps That 
+            From Concept to Production  — Rapidly.    We Build 
           </span>
 
           <span className="hero-line block mt-2">
-         Go Live and Drive Results. 
-           
+         Go Live and Drive <br />
+            <button ref={badgeRef}
+            className="inline-block mt-4 px-4 py-2 
+                       text-lg sm:text-xl md:text-2xl lg:text-2xl
+                       rounded-xl bg-emerald-600 text-white"> Results.</button>
              
         
            
